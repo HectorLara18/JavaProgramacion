945e621604697a5d15f621a08205fbf2ba958f97
Hector <hectorlaras.18@gmail.com>
2021-06-03 09:44:57 -0500

    se agrega ejemplo de polimorfismo en clase de test

--- SobreEscritura/src/test/ClasePrueba.java
+++ SobreEscritura/src/test/ClasePrueba.java
@@ -1,11 +1,13 @@
 package test;
 
-import domain.Gerente;
+import domain.*;
 
 public class ClasePrueba {
     public static void main(String[] args) {
         Gerente gerenteTI = new Gerente("Hector Lara", 57000.00);
+        Empleado ingeniero = new Empleado("Hector Lara", 35000.0);
         gerenteTI.setDepartamento("TI Desarrollo");
+        System.out.println(ingeniero.obtenerDetalles());
         System.out.println(gerenteTI.obtenerDetalles());
     }
 }
