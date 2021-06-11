a05c8f5128836ab4c16e27a968d95dc7ff0b87b8
Hector <hectorlaras.18@gmail.com>
2021-06-11 09:11:39 -0500

    se agrega nueva prueba

---
+++ APIColecciones/src/test/TestHashSet.java
@@ -0,0 +1,24 @@
+package test;
+import java.util.*;
+
+public class TestHashSet {
+    public static void main(String[] args) {
+        List lista1 = new ArrayList<>();
+        Set set1 = new HashSet();
+
+        lista1.add("Hdector");
+        lista1.add("Humberto");
+
+        set1.add("Manzana");
+        set1.add("Pera");
+
+        imprimie(lista1);
+        imprimie(set1);
+    }
+
+    public static void imprimie(Collection mycollection){
+        mycollection.forEach(element -> {
+            System.out.println("element = " + element);
+        });
+    }
+}
